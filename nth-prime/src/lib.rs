bfcee44bff762419ca8b672b22fe4519654d19f2
Tom Andrews <git@tomandrews.co.uk>
2020-04-12 19:28:47 +0100

    Solve the nth prime

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,23 @@
+fn is_prime(primes: &Vec<u32>, number: u32) -> bool {
+    for prime in primes {
+        if number % prime == 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut primes: Vec<u32> = Vec::new();
+
+    for i in 2..u32::max_value() {
+        if is_prime(&primes, i) {
+            if primes.len() == n as usize {
+                return i;
+            }
+            primes.push(i);
+        }
+    }
+
+    return 0;
 }

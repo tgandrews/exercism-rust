d9eebe751a1dc4fccadfb02b87e036c8a2002e63
Tom Andrews <git@tomandrews.co.uk>
2020-04-13 16:27:20 +0100

    Replace fold with sum

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,12 +1,9 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    (1..=n).into_iter().fold(0, |acc, v| acc + v).pow(2)
+    (1..=n).into_iter().sum::<u32>().pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1..=n)
-        .into_iter()
-        .map(|v| v.pow(2))
-        .fold(0, |acc, v| acc + v)
+    (1..=n).into_iter().map(|v| v.pow(2)).sum()
 }
 
 pub fn difference(n: u32) -> u32 {

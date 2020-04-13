5b1764e96716e34245e5cf5c0edb5284b7bded3d
Tom Andrews <git@tomandrews.co.uk>
2020-04-13 16:56:34 +0100

    Use iterator::any instead of loop

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,16 +1,11 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     (1..limit)
         .into_iter()
-        .filter(|i| {
-            for factor in factors {
-                if factor == &0 {
-                    continue;
-                }
-                if i % factor == 0 {
-                    return true;
-                }
-            }
-            return false;
+        .filter(|&i| {
+            factors
+                .iter()
+                .filter(|&&factor| factor > 0)
+                .any(|factor| i % factor == 0)
         })
         .sum()
 }
